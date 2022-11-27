f9febf9d126b462b0aaf904c266391f566e5f4ee
elis-11 <eliza.arzanukaeva@gmail.com>
2022-11-27 22:49:13 +0100

    Active Index

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,31 @@
 import { Products } from "./components/Products";
 import products from "./assets/products.json";
+import { useState } from "react";
 console.log(products);
 
 function App() {
+  // const [activeIndex, setActiveIndex] = useState(0);
+  // const categories = ["vegetable", "fruit", "berry", "cheese"];
+  // const activeCategory = (index) => {
+  //   setActiveIndex(index);
+  // };
 
   return (
     <div className="collumn items-center">
       <div className="flex  items-center font-bold mx-auto my-3">
         <h2 className="">Fridge App</h2>
       </div>
+      {/* <div className="categories flex m-5">
+        {categories.map((value, index) => (
+          <div 
+          // key={categories.length}
+            onClick={() => activeCategory(index)}
+            className={activeIndex === index ? "active" : "" } className="bg-gray-300 text-white p-3 m-3 rounded-2xl opacity-50 cursor-pointer"
+          >
+            {value}
+          </div>
+        ))}
+      </div> */}
       <div className="flex justify-between ">
         <div className=" grid grid-cols-4 gap-1 content-start bg-gray-300">
           {products.map((product) => (
