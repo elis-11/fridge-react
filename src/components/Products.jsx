da55c0b9caeda947d54f8afda510ec9f479aa633
elis-11 <eliza.arzanukaeva@gmail.com>
2022-11-27 22:08:02 +0100

    Render Products from products.json

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,30 +1,44 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
-export const Products = ({products}) => {
-const [count, setCount] = useState(0)
+export const Products = ({title, price, image, category, status, weight}) => {
+  const [count, setCount] = useState(0);
 
+  // const addProduct = () => {
+  //   setCount(count + 1);
+  // };
+  // const removeProduct = () => {
+  //   setCount(count - 1);
+  // };
 
   return (
     <div>
-        <div className=" grid grid-cols-4 gap-1 content-start bg-gray-300">
-          {products.map((product) => (
-            <div key={product._id} className="w-48 m-2 bg-orange-100 shadow-sm border-2 rounded border-red-100">
-              <img src={product.image} className="w-full h-24" alt={product.name} />
-              <div className="data p-4">
-                <div className="title">{product.title}</div>
-                <div className="weight">weight: {product.weight} gr.</div>
-                <div className="category">category: {product.cotegory}</div>
-                <div className="status">status: {product.status}</div>
-                <div className="price">price: {product.price}</div>
-                <div className="flex justify-center">
-                  <button className="bg-green-400 w-8 hover:bg-green-500">+</button>
-                  <div className="count px-3">{count}</div>
-                  <button className="bg-green-400 w-8 hover:bg-green-500">-</button>
-                </div>
-              </div>
-            </div>
-          ))}
+      <div
+        className="w-48 m-2 bg-orange-100 shadow-sm border-2 rounded border-red-100"
+      >
+        <img src={image} className="w-full h-24" alt={title} />
+        <div className="data p-4">
+          <div className="title">{title}</div>
+          <div className="weight">weight: {weight} gr.</div>
+          <div className="category">category: {category}</div>
+          <div className="status">status: {status}</div>
+          <div className="price">price: {price}</div>
+          <div className="flex justify-center">
+            <button
+              // onClick={addProduct}
+              className="bg-green-300 w-8 hover:bg-green-500"
+            >
+              +
+            </button>
+            <div className="count px-3">{count}</div>
+            <button
+              // onClick={removeProduct}
+              className="bg-green-300 w-8 hover:bg-green-500"
+            >
+              -
+            </button>
+          </div>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
