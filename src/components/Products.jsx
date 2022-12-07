583b2fd2cadb74c284c8869d80bb046a4b267841
elis-11 <eliza.arzanukaeva@gmail.com>
2022-12-07 14:35:50 +0100

    Status: in - out

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -7,6 +7,8 @@ export const Products = ({
   removeProduct,
   updateProductPrice,
 }) => {
+  const statusNames = ["inbox", "outbox"];
+
   return (
     <div>
       <div className="w-48 m-2 bg-orange-100 shadow-lg border-2 rounded border-red-100">
@@ -19,17 +21,7 @@ export const Products = ({
           <div className="title font-bold">{product.title}</div>
           <div className="weight">weight: {product.weight} gr.</div>
           <div className="category">category: {product.category}</div>
-          <div className="status">status: {product.status}</div>
           <div className="volume">volume: {product.volume}</div>
-          <div className="sizes flex">
-            <span>
-              {product.sizes.map((size) => (
-                <span key={size} className="mr-1 bg-orange-50">
-                  {size} sm.
-                </span>
-              ))}
-            </span>
-          </div>
           <div
             onClick={() => updateProductPrice(product._id)}
             className="price cursor-pointer"
@@ -37,6 +29,24 @@ export const Products = ({
             price: {product.price}
           </div>
 
+          <div className="status">
+            <span className="flex justify-between">
+              {product.status.map((statId) => (
+                <span key={statId} className="px-3 mb-1 bg-orange-50">
+                  {statusNames[statId]}
+                </span>
+              ))}
+            </span>
+          </div>
+          <div className="sizes flex">
+            <span className="flex justify-between">
+              {product.sizes.map((size) => (
+                <span key={size} className="mr-1 bg-orange-50">
+                  {size} sm.
+                </span>
+              ))}
+            </span>
+          </div>
           <div className="flex justify-center">
             <button
               onClick={() => addProduct(product._id)}
